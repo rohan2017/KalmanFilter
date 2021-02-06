6b9328fccab8371319bf002a18b3f552a3846d84
rohan2017 <rohanbandaru1@gmail.com>
2021-02-06 16:20:54 -0500

    Separate updating the filter from getting the angle
    This enables using a Kalman filter without automatically integrating the variable.

--- Kalman.h
+++ Kalman.h
@@ -23,7 +23,8 @@ public:
     Kalman();
 
     // The angle should be in degrees and the rate should be in degrees per second and the delta time in seconds
-    float getAngle(float newAngle, float newRate, float dt);
+	void update(float newAngle, float newRate, float dt);
+    float getAngle();
 
     void setAngle(float angle); // Used to set angle, this should be set as the starting angle
     float getRate(); // Return the unbiased rate
